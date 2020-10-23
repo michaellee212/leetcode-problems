8f07d10674745fea26c0e0e9ab6c0d25368969ed
unknown <michaelee212@gmail.com>
2020-10-23 10:24:03 -0700

    small changes

--- 3-Longest Substring Without Repeating Characters.js
+++ 3-Longest Substring Without Repeating Characters.js
@@ -26,13 +26,13 @@
 
 var lengthOfLongestSubstring = function(s) {
     var start = 0, maxLen = 0;
-    var map = new Map();
+    var store = new Map();
   
     for(var i = 0; i < s.length; i++) {
         var ch = s[i];
       
-        if(map.get(ch) >= start) start = map.get(ch) + 1;
-        map.set(ch, i);
+        if(store.get(ch) >= start) start = store.get(ch) + 1;
+        store.set(ch, i);
       
         if(i - start + 1 > maxLen) maxLen = i - start + 1;
     }
