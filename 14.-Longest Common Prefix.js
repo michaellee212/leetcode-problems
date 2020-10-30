b1873ea3a8fe6836778ba1bbf8ab20a103f8b715
unknown <michaelee212@gmail.com>
2020-10-29 23:33:48 -0700

    update to problem

--- 14.-Longest Common Prefix.js
+++ 14.-Longest Common Prefix.js
@@ -16,13 +16,15 @@
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lower-case English letters.
 
-// if there are no items in strs
-if (strs.length === 0) return '';
-let r = '';
-for (let i = 0; i < strs[0].length; i++) {
-  for (let j = 1; j < strs.length; j++) {
-    if (strs[0][i] !== strs[j][i]) return r;
-  }
-  r += strs[0][i];
-}
-return r;
+var longestCommonPrefix = function(strs) {
+    // if there are no items in strs
+      if (strs.length === 0) return '';
+      let r = '';
+      for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+          if (strs[0][i] !== strs[j][i]) return r;
+        }
+        r += strs[0][i];
+      }
+      return r;
+    }
