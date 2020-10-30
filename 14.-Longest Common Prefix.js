e0c5d8032ab1c1693d6591fb43bbfcf33be44fbb
unknown <michaelee212@gmail.com>
2020-10-29 23:33:21 -0700

    update to problem

---
+++ 14.-Longest Common Prefix.js
@@ -0,0 +1,28 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+ 
+// Constraints:
+// 0 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lower-case English letters.
+
+// if there are no items in strs
+if (strs.length === 0) return '';
+let r = '';
+for (let i = 0; i < strs[0].length; i++) {
+  for (let j = 1; j < strs.length; j++) {
+    if (strs[0][i] !== strs[j][i]) return r;
+  }
+  r += strs[0][i];
+}
+return r;
