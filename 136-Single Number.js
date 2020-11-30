fc7a557f0b646f22bb551dbf4fa05868bed2429e
unknown <michaelee212@gmail.com>
2020-11-29 23:05:34 -0800

    added new problem

--- 136-Single Number.js
+++ 136-Single Number.js
@@ -19,3 +19,7 @@
 // 1 <= nums.length <= 3 * 104
 // -3 * 104 <= nums[i] <= 3 * 104
 // Each element in the array appears twice except for one element which appears only once.
+
+var singleNumber = function(nums) {
+	return nums.reduce((prev, curr) => prev ^ curr);
+};
