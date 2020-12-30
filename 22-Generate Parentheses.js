9a10729724068dc97dcb520a07137e10c29ed3bf
unknown <michaelee212@gmail.com>
2020-12-30 12:30:53 -0800

    updates

---
+++ 22-Generate Parentheses.js
@@ -0,0 +1,39 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+ 
+
+// Constraints:
+
+// 1 <= n <= 8
+
+
+var generateParenthesis = function(n) {
+    
+    let result = [];
+    
+    function go(str, closedLeft, openLeft) {
+        if (str.length === n*2) {
+            result.push(str);
+        }
+        if (openLeft > 0) {
+            go(str + '(', closedLeft + 1, openLeft - 1);
+        }
+        if (closedLeft > 0) {
+            go(str + ')', closedLeft - 1, openLeft);
+        }
+    }
+   
+    go("", 0, n);
+    return result;
+    
+};
