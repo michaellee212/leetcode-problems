1d0906c0cf45fe2b55f8a96644679b932cbf0b18
unknown <michaelee212@gmail.com>
2021-03-05 15:23:11 -0800

    new problem

--- 78-Subsets.js
+++ 78-Subsets.js
@@ -10,3 +10,16 @@
 // Output: [[],[0]]
 
 
+var subsets = function(nums) {
+    let result = [];
+    dfs([], 0);
+    
+    function dfs(current, index){
+        result.push(current);
+        for(let i = index; i < nums.length; i++) {
+            dfs(current.concat(nums[i]), i + 1);
+        }
+    }
+    
+    return result;
+};
