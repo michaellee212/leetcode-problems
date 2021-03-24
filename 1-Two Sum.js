0668c05f129bf81addf9a5c78ffb0b134477d6b1
unknown <michaelee212@gmail.com>
2021-03-24 12:54:56 -0700

    updates

--- 1-Two Sum.js
+++ 1-Two Sum.js
@@ -73,7 +73,9 @@ const twoSum2 = (nums, target) => {
 
 const twoSum = function(nums, target) {
   const comp = {};
+
   for(let i=0; i<nums.length; i++){
+    
       if(comp[nums[i] ]>=0){
           return [ comp[nums[i] ] , i]
       }
--- 9-Palindrome Number.js
+++ 9-Palindrome Number.js
@@ -36,4 +36,21 @@ var isPalindrome = function (x) {
     } 
     return false;
   }
+
+
+// if we cant turn it into a string
+  const isPalindrome = (x) => {
+    if (x < 0) {
+      return false;
+    }
+  
+    let reverse = 0;
+  
+    for (let i = x; i >= 1; i = Math.floor(i/10)) {
+      reverse = reverse*10 + i%10;
+    }
+  
+    return reverse === x;
+  
+  }
   
