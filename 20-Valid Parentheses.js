8a024a2a5bece2c85e767816263df06410c4a893
Michael Lee <michaelee212@gmail.com>
2024-09-13 11:46:59 -0700

    Update 20-Valid Parentheses.js

--- 20-Valid Parentheses.js
+++ 20-Valid Parentheses.js
@@ -1,10 +1,9 @@
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 // An input string is valid if:
-
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
- 
+
 // Example 1:
 // Input: s = "()"
 // Output: true
@@ -24,30 +23,29 @@
 // Example 5:
 // Input: s = "{[]}"
 // Output: true
- 
+
 // Constraints:
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
-var isValid = function(s) {
-    let map = {
-        ")": "(",
-        "]": "[",
-        "}": "{"
+var isValid = function (s) {
+  let map = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      arr.push(s[i]);
+    } else {
+      if (arr[arr.length - 1] === map[s[i]]) {
+        // remove from arr
+        arr.pop();
+      } else return false;
     }
-    let arr = [];
-    for(let i = 0; i < s.length; i ++){
-        if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
-            arr.push(s[i]);
-        }
-        else{
-            if(arr[arr.length - 1] === map[s[i]]){
-                // remove from arr
-                arr.pop();
-            }
-            else return false;
-        }
-    }
-    // if arr is empty
-    return arr.length === 0 ? true : false;
+  }
+
+  // if arr is empty
+  return arr.length === 0 ? true : false;
 };
