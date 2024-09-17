a320a24230d1054c8d34389188f500b7c7cee694
Michael Lee <michaelee212@gmail.com>
2024-09-17 15:54:14 -0700

    Update 14.-Longest Common Prefix.js

--- 14.-Longest Common Prefix.js
+++ 14.-Longest Common Prefix.js
@@ -10,33 +10,29 @@
 // Input: strs = ["dog","racecar","car"]
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
- 
+
 // Constraints:
 // 0 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lower-case English letters.
 
-var longestCommonPrefix = function(strs) {
-    // if there are no items in strs
-      if (strs.length === 0) return '';
-      
-      let r = '';
-      for (let i = 0; i < strs[0].length; i++) {
-        for (let j = 1; j < strs.length; j++) {
+var longestCommonPrefix = function (strs) {
+  // if there are no items in strs
+  if (strs.length === 0) return "";
 
-        //     .0 .1 .2  
-        
-        //  0   s  t  r 
+  let r = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      //     .0 .1 .2
+      //  0   s  t  r
+      //  1   s  t  r
+      //  2   s  r  v
 
-        //  1   s  t  r 
-
-        //  2   s  r  v 
-
-          if (strs[0][i] !== strs[j][i]) {
-            return r;
-          }
-        }
-        r += strs[0][i];
+      if (strs[0][i] !== strs[j][i]) {
+        return r;
       }
-      return r;
     }
+    r += strs[0][i];
+  }
+  return r;
+};
