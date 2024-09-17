37e993279bb08cac79e7d51f66d52188f0fc5c13
Michael Lee <michaelee212@gmail.com>
2024-09-17 15:53:50 -0700

    Update 1-Two Sum.js

--- 1-Two Sum.js
+++ 1-Two Sum.js
@@ -23,10 +23,8 @@
 
 const twoSum = (nums, target) => {
   const map = {};
-
   for (let i = 0; i < nums.length; i++) {
     const another = target - nums[i];
-
     if (another in map) {
       return [map[another], i];
     }
