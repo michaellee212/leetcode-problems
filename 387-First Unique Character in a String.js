de5987ca0da890b44130c3648ee618e7bb2e3432
Michael Lee <michaelee212@gmail.com>
2024-09-24 16:15:24 -0700

    Update 387-First Unique Character in a String.js

--- 387-First Unique Character in a String.js
+++ 387-First Unique Character in a String.js
@@ -7,28 +7,25 @@
 // s = "loveleetcode"
 // return 2.
 
-var firstUniqChar = function(s) {
-    
-    const ht = {};
-    
-    // fill in our object hash table with the character counts
-    for(let i = 0; i < s.length; i++) {
-        let item = s[i];
-        
-        ht[item] = ht[item] + 1 || 1;
-        
+var firstUniqChar = function (s) {
+  const ht = {};
+
+  // fill in our object hash table with the character counts
+  for (let i = 0; i < s.length; i++) {
+    let item = s[i];
+
+    ht[item] = ht[item] + 1 || 1;
+  }
+  console.log(ht);
+
+  // check if character count = 1 if so return that character
+  for (let i = 0; i < s.length; i++) {
+    let item = s[i];
+
+    if (ht[item] === 1) {
+      return i;
     }
-    
-    console.log(ht);
-    
-    // check if character count = 1 if so return that character
-    for(let i = 0; i < s.length; i++) {
-        let item = s[i];
-        
-        if(ht[item] === 1) {
-            return i;
-        }
-    }
-    
-    return -1;
+  }
+
+  return -1;
 };
